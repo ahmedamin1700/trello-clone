a789e7828f8d3c72666da27abeda0667a6fd1295
Ahmed Amin <ahmedamin1700@gmail.com>
2022-06-06 09:46:54 +0000

    initial

---
+++ src/Column.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from "react";
+import { AddNewItem } from "./AddNewItem";
+import { ColumnContainer, ColumnTitle } from "./styles";
+
+type ColumnProps = React.PropsWithChildren<{
+  text: string;
+}>;
+
+export const Column: FC<ColumnProps> = ({ text, children }) => {
+  return (
+    <ColumnContainer>
+      <ColumnTitle>{text}</ColumnTitle>
+      {children}
+      <AddNewItem
+        toggleButtonText="+ Add another list"
+        onAdd={console.log}
+        dark
+      />
+    </ColumnContainer>
+  );
+};
